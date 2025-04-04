afad7ccc9115d9808c801185d9e484b6dc5eeb13
Denkata-Iliev <denislav9711@gmail.com>
2025-04-04 16:29:04 +0300

    Make working countdown timer for TimedStudyScreen

--- app/src/main/java/com/example/flashcardsapp/ui/study/TimedStudyScreen.kt
+++ app/src/main/java/com/example/flashcardsapp/ui/study/TimedStudyScreen.kt
@@ -1,5 +1,6 @@
 package com.example.flashcardsapp.ui.study
 
+import androidx.compose.animation.Crossfade
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -24,6 +25,11 @@ import androidx.compose.material3.ProgressIndicatorDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
@@ -38,7 +44,7 @@ import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.flashcardsapp.R
 import com.example.flashcardsapp.ui.CustomFactories
-import com.example.flashcardsapp.ui.theme.FlashCardsAppTheme
+import kotlinx.coroutines.delay
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -49,6 +55,9 @@ fun TimedStudyScreen(
         factory = CustomFactories.timedStudyFactory(deckId)
     )
 ) {
+    val seconds by viewModel.seconds
+    val progress by viewModel.progress
+
     Scaffold(
         topBar = {
             CenterAlignedTopAppBar(
@@ -68,48 +77,116 @@ fun TimedStudyScreen(
             )
         }
     ) { padding ->
-        Column(
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally,
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(
-                    top = 0.dp,
-                    bottom = padding.calculateBottomPadding().plus(80.dp),
-                    start = padding.calculateLeftPadding(LayoutDirection.Ltr),
-                    end = padding.calculateLeftPadding(LayoutDirection.Ltr),
-                ),
-        ) {
-            CountdownTimer(
-                progress = .5f,
-                strokeWidth = 8.dp,
-                modifier = Modifier.size(150.dp),
-            )
+        var hasStarted by remember { mutableStateOf(false) }
 
-            Spacer(modifier = Modifier.height(24.dp))
+        var answerShown by remember { mutableStateOf(false) }
+        var state by remember { mutableStateOf(CardFace.Front) }
+        var shouldChangeCard by remember { mutableStateOf(false) }
 
-            FlipCard(
-                cardFace = CardFace.Front,
-                axis = RotationAxis.AxisX,
-                back = { /* Back Content */ },
-                front = {
-                    CardText(text = "questionText")
-                },
+        Crossfade(hasStarted) { currentState ->
+            if (!currentState) {
+                Box(
+                    contentAlignment = Alignment.Center,
+                    modifier = Modifier.fillMaxSize()
+                ) {
+                    Button(onClick = { hasStarted = true }) {
+                        Text(text = "Start")
+                    }
+                }
+
+                return@Crossfade
+            }
+
+            Column(
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally,
                 modifier = Modifier
-                    .fillMaxWidth()
-                    .aspectRatio(1.25f)
-                    .padding(8.dp)
-            )
-
-            Spacer(modifier = Modifier.height(24.dp))
-
-            Button(
-                onClick = { },
-                modifier = Modifier.fillMaxWidth(0.6f)
+                    .fillMaxSize()
+                    .padding(
+                        top = 0.dp,
+                        bottom = padding.calculateBottomPadding(),
+                        start = padding.calculateLeftPadding(LayoutDirection.Ltr),
+                        end = padding.calculateLeftPadding(LayoutDirection.Ltr),
+                    ),
             ) {
-                Text(stringResource(R.string.show_answer))
+                LaunchedEffect(answerShown) {
+                    if (!answerShown) {
+                        viewModel.startTimer()
+                    }
+                }
+
+                CountdownTimer(
+                    remainingSeconds = seconds / 1000L,
+                    progress = progress,
+                    strokeWidth = 8.dp,
+                    modifier = Modifier.size(150.dp),
+                )
+
+                Spacer(modifier = Modifier.height(24.dp))
+
+                FlipCard(
+                    cardFace = state,
+                    axis = RotationAxis.AxisY,
+                    back = {
+                        CardText(text = "answer")
+                    },
+                    front = {
+                        CardText(text = "question")
+                    },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .aspectRatio(1.25f)
+                        .padding(8.dp)
+                )
+
+                Spacer(modifier = Modifier.height(24.dp))
+
+                if (answerShown) {
+                    TimedStudyButton(
+                        text = "Next",
+                        onClick = {
+                            viewModel.stopTimer()
+
+                            state = state.next
+                            shouldChangeCard = true
+                            answerShown = false
+                        },
+                    )
+                } else {
+                    TimedStudyButton(
+                        text = stringResource(R.string.show_answer),
+                        onClick = {
+                            viewModel.stopTimer()
+
+                            state = state.next
+                            answerShown = true
+                        },
+                    )
+                }
+
+                LaunchedEffect(state) {
+                    if (state == CardFace.Front && shouldChangeCard) {
+                        delay(100)
+//                    viewModel.removeCardFromSession(card!!)
+//                    card = uiState.cards.randomOrNull()
+                    }
+                }
             }
         }
+    }
+}
+
+@Composable
+fun TimedStudyButton(
+    text: String,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Button(
+        onClick = onClick,
+        modifier = modifier.fillMaxWidth(0.6f)
+    ) {
+        Text(text = text)
     }
 }
 
@@ -117,6 +194,7 @@ fun TimedStudyScreen(
 fun CountdownTimer(
     progress: Float,
     modifier: Modifier = Modifier,
+    remainingSeconds: Long = 10L,
     foregroundColor: Color = MaterialTheme.colorScheme.primary,
     backgroundColor: Color = MaterialTheme.colorScheme.secondary.copy(alpha = 0.3f),
     strokeWidth: Dp = ProgressIndicatorDefaults.CircularStrokeWidth,
@@ -142,78 +220,9 @@ fun CountdownTimer(
         )
 
         Text(
-            text = "${(progress * 100).toInt()}%",
+            text = "${remainingSeconds}s",
             style = MaterialTheme.typography.headlineMedium,
             fontWeight = FontWeight.Bold
         )
     }
-}
-
-@OptIn(ExperimentalMaterial3Api::class)
-@Preview
-@Composable
-private fun t() {
-    Scaffold(
-        topBar = {
-            CenterAlignedTopAppBar(
-                title = { Text(text = stringResource(R.string.app_name)) },
-                navigationIcon = {
-                    IconButton(
-                        onClick = {  }
-                    ) {
-                        Icon(
-                            imageVector = Icons.AutoMirrored.Filled.ArrowBack,
-                            contentDescription = stringResource(R.string.back_arrow),
-                            Modifier.clickable {  }
-                        )
-                    }
-                },
-                modifier = Modifier.shadow(elevation = dimensionResource(R.dimen.top_app_bar_elevation))
-            )
-        }
-    ) { padding ->
-        Column(
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally,
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(
-                    top = 0.dp,
-                    bottom = padding.calculateBottomPadding().plus(80.dp),
-                    start = padding.calculateLeftPadding(LayoutDirection.Ltr),
-                    end = padding.calculateLeftPadding(LayoutDirection.Ltr),
-                ),
-        ) {
-            CountdownTimer(
-                progress = .5f,
-                strokeWidth = 8.dp,
-                modifier = Modifier.size(150.dp),
-            )
-
-            Spacer(modifier = Modifier.height(24.dp))
-
-            FlipCard(
-                cardFace = CardFace.Front,
-                axis = RotationAxis.AxisX,
-                back = { /* Back Content */ },
-                front = {
-                    CardText(text = "questionText")
-                },
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .aspectRatio(1.25f)
-                    .padding(8.dp)
-            )
-
-            Spacer(modifier = Modifier.height(24.dp))
-
-            Button(
-                onClick = { },
-                modifier = Modifier.fillMaxWidth(0.6f)
-            ) {
-                Text(stringResource(R.string.show_answer))
-            }
-        }
-    }
-
 }
--- app/src/main/java/com/example/flashcardsapp/ui/study/TimedStudyViewModel.kt
+++ app/src/main/java/com/example/flashcardsapp/ui/study/TimedStudyViewModel.kt
@@ -1,10 +1,40 @@
 package com.example.flashcardsapp.ui.study
 
+import android.os.CountDownTimer
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.mutableLongStateOf
 import androidx.lifecycle.ViewModel
 import com.example.flashcardsapp.data.repository.CardRepository
+import java.util.concurrent.TimeUnit
 
 class TimedStudyViewModel(
     private val deckId: Int,
     private val cardRepository: CardRepository
 ) : ViewModel() {
+    var progress = mutableFloatStateOf(1f)
+        private set
+
+    var seconds = mutableLongStateOf(TimeUnit.SECONDS.toMillis(10))
+        private set
+
+    private val countDownTimer: CountDownTimer = object : CountDownTimer(seconds.longValue, 50L) {
+        override fun onTick(timeLeft: Long) {
+            seconds.longValue = timeLeft
+            progress.floatValue = timeLeft.toFloat() / 10_000f
+        }
+
+        override fun onFinish() {
+            progress.floatValue = 0f
+        }
+    }
+
+    fun startTimer() {
+        countDownTimer.start()
+    }
+
+    fun stopTimer() {
+        progress.floatValue = 1f
+        countDownTimer.cancel()
+        seconds.longValue = TimeUnit.SECONDS.toMillis(10)
+    }
 }
